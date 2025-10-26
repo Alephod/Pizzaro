5325f00b3fac8d769b3789130c3b3945a9306af0
Alephod <gimadiev0212@yandex.ru>
2025-10-26 17:06:39 +0300

    chore: add ignore path to eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
     ...compat.extends('next/core-web-vitals', 'next/typescript'),
     {
-        ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'dist/**', 'public/**', 'coverage/**', 'prisma/generated/**', 'next.config.ts'],
+        ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'dist/**', 'public/**', 'coverage/**', 'prisma/generated/**', 'next.config.ts', 'src/generated/**'],
     },
     {
         rules: {
