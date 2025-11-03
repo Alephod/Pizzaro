d50d2e485f41ed41230cbf5d65a3967338417260
Alephod <gimadiev0212@yandex.ru>
2025-11-04 00:09:32 +0300

    feat: logout and popup menu

--- frontend/src/app/admin/dashboard/DashboardAside.tsx
+++ frontend/src/app/admin/dashboard/DashboardAside.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { ArrowLeft, EllipsisVertical } from 'lucide-react';
+import { ArrowLeft, EllipsisVertical, LogOut, Settings } from 'lucide-react';
 import { Utensils } from 'lucide-react';
 import style from './layout.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState, type ElementType } from 'react';
+import { useEffect, useRef, useState, type ElementType } from 'react';
 import { usePathname } from 'next/navigation';
+import { signOut } from 'next-auth/react';
 
 interface AsideProps {
     username: string | null | undefined;
@@ -27,19 +28,32 @@ const navItems: NavItem[] = [
 ];
 
 export default function DashboardAside({ username }: AsideProps) {
-    const [isClosed, setIsClosed] = useState(false);
+    const [isAsideClosed, setIsAsideClosed] = useState(false);
+    const [isPopupMenuOpen, setIsPopupMenuOpen] = useState(false);
 
     const pathname = usePathname();
 
+    const popupMenuRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (popupMenuRef.current && e.target instanceof Node && !popupMenuRef.current.contains(e.target)) {
+                setIsPopupMenuOpen(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, []);
+
     return (
-        <aside className={style.aside + ' ' + (isClosed ? style.asideСlosed : '')}>
+        <aside className={style.aside + ' ' + (isAsideClosed ? style.asideClosed : '')}>
             <div className={style.header}>
                 <div className={style.logo}>
                     <Image alt="Pizzaro admin" src="/logo-admin.svg" width={150} height={60} />
                 </div>
                 <div className={style.arrowWrapper}>
                     <div className={style.arrow}>
-                        <ArrowLeft onClick={() => setIsClosed(!isClosed)} />
+                        <ArrowLeft onClick={() => setIsAsideClosed(!isAsideClosed)} />
                     </div>
                 </div>
             </div>
@@ -47,7 +61,7 @@ export default function DashboardAside({ username }: AsideProps) {
             {navItems.map(item => {
                 const Icon = item.icon;
                 return (
-                    <Link className={style.link} key={item.name} href={pathname + item.href}>
+                    <Link className={style.link} key={item.name} href={pathname + item.href} data-label={item.name}>
                         <Icon className={style.icon} />
                         <span>{item.name}</span>
                     </Link>
@@ -57,8 +71,22 @@ export default function DashboardAside({ username }: AsideProps) {
             <div className={style.profile}>
                 <div className={style.profileAvatar}></div>
                 <p>{username}</p>
-                <EllipsisVertical className={style.profileOptions} />
+                <div onClick={() => setIsPopupMenuOpen(!isPopupMenuOpen)}>
+                    <EllipsisVertical className={style.profileOptions} />
+                </div>
             </div>
+            {isPopupMenuOpen && (
+                <div ref={popupMenuRef} className={style.popupMenu}>
+                    <Link href={'/admin/settings'} className={style.menuItem}>
+                        <Settings size={22} />
+                        <span>Настройки</span>
+                    </Link>
+                    <button className={`${style.menuItem} ${style.menuItemExit}`} onClick={() => signOut({ callbackUrl: '/admin/login' })}>
+                        <LogOut size={22} />
+                        <span>Выйти</span>
+                    </button>
+                </div>
+            )}
         </aside>
     );
 }
